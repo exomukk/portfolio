a4f80594c22c41413139600cb59de941858bafcf
huythanthanh <trum.huy1036@gmail.com>
2022-08-10 21:02:14 +0700

    I've made things

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,10 +1,107 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+
+{/* Starting frontend  */}
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>              
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+          </div>
+        </div>
+{/* End of frontend codes */}
+{/* Start backend */}
+        <div className="experience_backend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>  Intermediate</small>
+              </div>    
+            </article>
+
+
+            <article className='experience_details'>
+              <BsPatchCheckFill className='experience_details-icon' />
+              <div>
+                <h4 className='h4'>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>    
+            </article>
+          </div>
+        </div>
+{/* End backend */}
+      </div>
     </section>
   )
 }
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -3,8 +3,13 @@ import './services'
 
 const Services = () => {
   return (
-    <section id='service'>
-      Services
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service"></article>
+      </div>
     </section>
   )
 }
